04ed4bc494f37125a43c7d4ed4e5dbac34f92c2c
SkOoLyD <79675305+SkOoLyD@users.noreply.github.com>
2021-03-28 23:19:26 -0500

    second part

--- script.js
+++ script.js
@@ -12,26 +12,23 @@ window.addEventListener("load",function() {
          console.log(json[index].name);
          document.getElementById("missionTarget").innerHTML =`<h2>Mission Destination</h2>
          <ol>
-            <li>Name: ${}</li>
-            <li>Diameter: ${}</li>
-            <li>Star: ${}</li>
-            <li>Distance from Earth: ${}</li>
-            <li>Number of Moons: ${}</li>
+            <li>Name: ${json[index.name]}</li>
+            <li>Diameter: ${json[index.diameter]}</li>
+            <li>Star: ${json[index].star}</li>
+            <li>Distance from Earth: ${json[index].distance}</li>
+            <li>Number of Moons: ${json[index].moons}</li>
          </ol>
-         <img src="${}">
+         <img src="${json[index].images}">
          `; 
 
       });
    });
+
+//make misson submit form 
+   let form = document.querySelector("form");
+form.addEventListener("submit", function(event){
+   let pilotNameInput = document.querySelector("input[name=pilotName]");
+   let coPilotNameInput = doucument.querySelector("input[name=coPilotName");
+   let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+   let cargoMassInput = document.querySelector("input[name=cargoMass]");
 }
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
